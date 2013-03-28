d6a45dcd12cda02029fce7a8af81b99b55401687
veit <kontakt@veit-schiele.de>
2013-03-28 11:28:08 +0100

    Added wpd.policy

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0.0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.rst').read()
+    + '\n' +
+    open('CHANGES.rst').read()
+    + '\n')
+
+setup(name='wpd.policy',
+      version=version,
+      description="Policy product for wpd sites",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Veit Schiele',
+      author_email='kontakt@veit-schiele.de',
+      url='https://github.com/veit/wpd.policy',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['wpd', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+          'wpd.countdown',
+      ],
+      extras_require={'test': ['plone.app.testing']},
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      setup_requires=["PasteScript"],
+      paster_plugins=["templer.localcommands"],
+      )
